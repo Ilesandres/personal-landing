cb604da4aaa05c9b64e97b4359ebed486b4e19f5
Ilesandres <ilesandres6@gmail.com>
2026-09-04 18:29:56 -0500

    feat(home): add hero with typed roles, live GitHub stats and featured projects

--- src/app/core/services/github.service.ts
+++ src/app/core/services/github.service.ts
@@ -7,31 +7,39 @@ const API = 'https://api.github.com';
 const USER = 'Ilesandres';
 const ORG = 'AIWaveSystems';
 
+const COMMITS_ACCEPT = 'application/vnd.github+json';
+
 @Injectable({ providedIn: 'root' })
 export class GithubService {
-	private readonly user$: Observable<GithubUser> = this.http
-		.get<GithubUser>(`${API}/users/${USER}`)
-		.pipe(shareReplay(1));
+	private readonly user$: Observable<GithubUser>;
+	private readonly org$: Observable<GithubOrg>;
+	private readonly userRepos$: Observable<GithubRepo[]>;
+	private readonly orgRepos$: Observable<GithubRepo[]>;
+	private readonly commits$: Observable<{ total_count: number }>;
 
-	private readonly org$: Observable<GithubOrg> = this.http
-		.get<GithubOrg>(`${API}/orgs/${ORG}`)
-		.pipe(shareReplay(1));
+	constructor(private readonly http: HttpClient) {
+		this.user$ = this.http
+			.get<GithubUser>(`${API}/users/${USER}`)
+			.pipe(shareReplay(1));
 
-	private readonly userRepos$: Observable<GithubRepo[]> = this.http
-		.get<GithubRepo[]>(`${API}/users/${USER}/repos?per_page=100&sort=pushed`)
-		.pipe(shareReplay(1));
+		this.org$ = this.http
+			.get<GithubOrg>(`${API}/orgs/${ORG}`)
+			.pipe(shareReplay(1));
 
-	private readonly orgRepos$: Observable<GithubRepo[]> = this.http
-		.get<GithubRepo[]>(`${API}/orgs/${ORG}/repos?per_page=100&sort=pushed`)
-		.pipe(shareReplay(1));
+		this.userRepos$ = this.http
+			.get<GithubRepo[]>(`${API}/users/${USER}/repos?per_page=100&sort=pushed`)
+			.pipe(shareReplay(1));
 
-	private readonly commits$: Observable<{ total_count: number }> = this.http
-		.get<{ total_count: number }>(`${API}/search/commits?q=author:${USER}&per_page=1`, {
-			headers: { Accept: 'application/vnd.github+json' },
-		})
-		.pipe(shareReplay(1));
+		this.orgRepos$ = this.http
+			.get<GithubRepo[]>(`${API}/orgs/${ORG}/repos?per_page=100&sort=pushed`)
+			.pipe(shareReplay(1));
 
-	constructor(private readonly http: HttpClient) {}
+		this.commits$ = this.http
+			.get<{ total_count: number }>(`${API}/search/commits?q=author:${USER}&per_page=1`, {
+				headers: { Accept: COMMITS_ACCEPT },
+			})
+			.pipe(shareReplay(1));
+	}
 
 	getUser(): Observable<GithubUser> {
 		return this.user$;
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { combineLatest, map } from 'rxjs';
+import { PROFILE } from '../../core/data/profile';
+import { GithubRepo } from '../../core/models/github';
+import { GithubService } from '../../core/services/github.service';
+import { bannerFor } from '../../core/utils/banner';
+import { TypedTextComponent } from './typed-text.component';
 
 @Component({
 	standalone: true,
 	selector: 'app-home',
+	imports: [AsyncPipe, RouterLink, TypedTextComponent],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+	private readonly github = inject(GithubService);
+
+	protected readonly profile = PROFILE;
+	protected readonly bannerFor = bannerFor;
+	protected readonly featuredNames = ['personal-landing', 'Tienda-react-sql-node', 'prueba-front-donarApp', 'game-recolections'];
+
+	roles = ['Desarrollador Backend', 'Desarrollador Fullstack', 'Ingeniero de Sistemas en formación'];
+
+	readonly user$ = this.github.getUser();
+	readonly stats$ = combineLatest([this.github.getUser(), this.github.getCommitCount()]).pipe(
+		map(([user, commits]) => [
+			{ label: 'Repos públicos', value: user.public_repos, hint: 'en mi GitHub' },
+			{ label: 'Commits públicos', value: commits, hint: 'subidos a GitHub' },
+			{ label: 'Seguidores', value: user.followers, hint: 'y contando' },
+		]),
+	);
+
+	readonly featured$ = this.github.getUserRepos().pipe(
+		map((repos) =>
+			this.featuredNames
+				.map((name) => repos.find((repo) => repo.name === name))
+				.filter((repo): repo is GithubRepo => Boolean(repo)),
+		),
+	);
+}
